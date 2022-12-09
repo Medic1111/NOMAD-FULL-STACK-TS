64ba7df636f4ccec0725458d84c7b20f2dc69149
Aryse Tansy <medictansy@gmail.com>
2022-12-08 21:40:07 -0500

    Bug Fix: Default URL Not applying on Register

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,10 +3,15 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const registerHandler = async (req, res) => {
-  const { email, username, password, avatar } = req.body;
+  let { email, username, password, avatar } = req.body;
 
   if (!username || !email || password.length < 6) {
     return res.status(422).json({ message: "Invalid credentials" });
+  }
+
+  if (avatar === "") {
+    avatar =
+      "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=";
   }
 
   User.findOne(
